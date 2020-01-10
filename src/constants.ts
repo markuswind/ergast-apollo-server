f068fcbfa100243eb3787232b037ca5041925ba0
Markus Wind <markuswind@gmail.com>
2020-01-10 09:52:34 +0100

    feat: added cache utils to restdatasource

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
 export const BASE_URLS = {
   ergast: 'https://ergast.com/api/f1/'
 };
+
+export const CACHE_TIMES = {
+  minutes: 600,
+  day: 86400,
+  month: 2592000
+};
--- src/modules/qualifyingResults/provider.ts
+++ src/modules/qualifyingResults/provider.ts
@@ -3,6 +3,7 @@ import { RestDataSource } from '../../utils/RestDataSource';
 
 export class QualifyingResultsProvider extends RestDataSource {
   // MARK: api calls
+
   public async getQualifyingResults(args: QueryQualifyingResultsArgs) {
     const result = await this.get(`${args.year}/${args.round}/qualifying.json`);
     return this.parseRaceTable(result);
